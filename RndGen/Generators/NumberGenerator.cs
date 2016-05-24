9559c72c8d719bad102f2458bea90659f692a840
Jukata <imag1nabl3@gmail.com>
2016-05-24 20:40:46 +0300

    Add project files.

---
+++ RndGen/Generators/NumberGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RndGen.Generators
+{
+    public class NumberGenerator
+    {
+
+        // int generator
+        public static int[] GenerateNumbers (int limit, int number = 1)
+        {
+            int[] generatedNumbers = new int[number];
+            Random rn = new Random();
+
+            for (int i = 0; i < generatedNumbers.Length; i++)
+            {
+                generatedNumbers[i] = rn.Next(limit);
+            }
+
+            return generatedNumbers;
+        }
+
+        // double generator
+        public static double[] GenerateNumbers(double upperLimit, int number = 1)
+        {
+            double[] generatedNumbers = new double[number];
+            Random rn = new Random();
+
+            for (int i = 0; i < generatedNumbers.Length; i++)
+            {
+                string num = rn.Next((int)upperLimit).ToString() + "." + rn.Next(9);
+                generatedNumbers[i] = Convert.ToDouble(num);
+            }
+            return generatedNumbers;
+        }
+    }
+}
---
+++ RndGen/Generators/StringGenerator.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RndGen.Generators
+{
+    public static class StringGenerator
+    {
+        private static char[] letters = new char[]
+        {
+            'a',
+            'b',
+            'c',
+            'd',
+            'e',
+            'f',
+            'g',
+            'h',
+            'i',
+            'j',
+            'k',
+            'l',
+            'm',
+            'n',
+            'o',
+            'p',
+            'q',
+            'r',
+            's',
+            't',
+            'u',
+            'v',
+            'w',
+            'x',
+            'y',
+            'z'
+        };
+        
+        // Text generator
+        public static string TextGenerator(int numberOfWords, int wordLowerLimit = 2, int wordUpperLimit = 10)
+        {
+            string[] wordsContainer = new string[numberOfWords];
+            Random rnd = new Random();
+            
+            for (int i = 0; i < wordsContainer.Length; i++)
+            {
+                int wordLength = rnd.Next(wordLowerLimit, wordUpperLimit);
+                string word = "";
+                for (int j = 0; j < wordLength; j++)
+                {
+                    char randomChar = letters[rnd.Next(letters.Length)];
+                    word += randomChar.ToString();
+                }
+            }
+
+            return string.Join(" ", wordsContainer);
+        }
+
+        //  Single Word Generator
+        public static string WordGenerator (int wordLowerLimit = 2, int wordUpperLimit = 10)
+        {
+            string word = "";
+            Random rnd = new Random();
+            int wordLength = rnd.Next(wordLowerLimit, wordUpperLimit);
+            for (int i = 0; i < wordLength; i++)
+            {
+                char randomChar = letters[rnd.Next(letters.Length)];
+                word += randomChar.ToString();
+            }
+
+            return word;
+        }
+    }
+}
